f99753e2e0e1f71e61bf5cc8710650343288b303
Andrew Davis <atd285@gmail.com>
2021-03-01 20:22:46 -0500

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import protobuf from 'protobufjs';
+const Buffer = require('buffer/');
+
+const emojis = {
+  '': '',
+  up: '🚀',
+  down: '💩',
+};
+
+function formatPrice(price: number) {
+  return `$${price.toFixed(2)}`;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [stock, setStock] = useState<[] | null>(null);
+  const [direction, setDirection] = useState<string>('');
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    protobuf.load('./YPricingData.proto', (error, root) => {
+      if (error) {
+        return console.error(error);
+      }
+      const Yaticker = root?.lookupType('yaticker');
+
+      ws.onopen = function open() {
+        console.log('connected');
+        ws.send(
+          JSON.stringify({
+            subscribe: [(params.get('symbol') || 'GME').toUpperCase()],
+          })
+        );
+      };
+      ws.onclose = function closed() {
+        console.log('disconnected');
+      };
+
+      ws.onmessage = function incoming(message) {
+        const next = Yaticker?.decode(new Buffer(message.data, 'base64'));
+      };
+    });
+  });
+  return <h1>temp</h1>
 }
 
 export default App;
